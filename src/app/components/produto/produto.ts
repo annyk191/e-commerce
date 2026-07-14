349cf18cd3dc44c414eaeb723fc92309799401e3
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-07-13 21:42:54 -0300

    feat : src/app/components/produto/produto.ts

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+// adicionando a classe produto com as propriedades produto, 
+export class Produto {
+  produto = 'notebook gamer'
+  preco = 1500.00;
+  mostrarProduto = true;
+  mostrarPreco = true;
+produtos =[
+  {nome:'Teclado', preco:'49.99'},
+  {nome:'Mouse', preco:'29.99'},
+  {nome:'Monitor',preco:'149.99'},
+]
+}
