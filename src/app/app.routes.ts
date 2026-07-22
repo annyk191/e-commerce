b80250fb0029fa6139b424fffaf98dd8b349e34b
Anny karolinny <annykarolinnynascimento@gmail>
2026-07-22 18:49:18 -0300

    refactor:  src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
 import { ListaProdutos}from './features/produtos/lista-produtos/lista-produtos';
 import { Carrinho } from './features/carrinho/carrinho/carrinho';
+import { Home } from './features/home/home/home';
 
 export const routes: Routes = [
     {
         path:'',
+          component: Home,
+    },
+    {
+        path:'produto',
         component: ListaProdutos,
     },
     {
          path:'carrinho',
           component: Carrinho,
-    }
+    },
+    {
+        path:'**',
+          redirectTo:'',
+    },
 ];
