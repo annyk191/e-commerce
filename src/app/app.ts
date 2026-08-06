71748c820ed87f1c54b05afed79c653c7f1bc52b
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-08-05 21:51:57 -0300

    refactor: src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router'; 
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
+import { LowerCasePipe } from '@angular/common';
 import { usuarioLogado, login, logout } from './core/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe ],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, MatCardModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('e-commerce');
-  nomeloja = 'Mercado da Anny';
+  protected readonly title = signal('e-commerce-aluno');
+  nomeLoja = 'Mercado';
   usuarioLogado = usuarioLogado;
-  login =login;
-  logout =logout;
+  login = login;
+  logout = logout;
 }
