ea899316346ca75cc24b6f87eb6f78a9067dfd75
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-08-14 21:01:56 -0300

    Refactor: src/app/core/interceptors/http.interceptor.ts

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -2,11 +2,13 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { tap, catchError, throwError } from 'rxjs';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.servise';
+import { Router } from '@angular/router';
 
 export const HttpInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('Interceptando requisição:', req.url);
 
   const authService = inject(AuthService);
+  const router = inject(Router);
   const token = authService.obterToken();
 
   const novaReq = token
@@ -27,14 +29,20 @@ export const HttpInterceptor: HttpInterceptorFn = (req, next) => {
       console.error('ERRO GLOBAL:', error);
 
       if (error.status === 401) {
-        console.warn(
+        console.error(
           'Erro de autenticação de usuário:',
           error
         );
+        authService.logout();
+        router.navigateByUrl('/login');
       }
 
       if (error.status === 500) {
         console.warn('Erro interno do servidor!');
+      }
+      if(error.status === 403){
+        console.warn('acesso proibido, Usuário sem permissão!');
+        router.navigateByUrl('/produtos');
       }
 
       return throwError(() => error);
