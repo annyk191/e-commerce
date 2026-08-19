64909f103a2af345992c8cad20ba17899d587a4e
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-08-18 21:37:40 -0300

    Feat:  src/app/core/guards/

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn } from "@angular/router";
+import { AuthService } from "../services/auth.servise";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+
+    export const authGuard: CanActivateFn = () => {
+        
+        const authService = inject(AuthService);
+        const router = inject(Router);
+
+        if (authService.usuarioLogado()){
+            return true;
+        }
+        return router.createUrlTree(['/login']);
+    };
