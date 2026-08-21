5f1de7e2816531f2af9d4c660f71e22c942238ff
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-08-21 20:43:57 -0300

    Refactor:src/app/core/models/pedido-finalizado.ts

--- src/app/core/models/pedido-finalizado.ts
+++ src/app/core/models/pedido-finalizado.ts
@@ -2,6 +2,7 @@ import { ItemCarrinho } from "./item-carrinho";
 
 export type pedidoFinalizado ={
     codigo:number;
+    email:string;
     cliente:string;
     quantidadeItens:number;
     total:number;
