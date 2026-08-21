0ce9af228045026795fedd904d9a9b694de30ecd
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-08-21 18:53:23 -0300

    Refactor:src/app/core/guards/auth.guard.ts

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,11 +1,11 @@
 import { CanActivateFn } from "@angular/router";
-import { AuthService } from "../services/auth.servise";
+import { AuthFacade } from "../facades/auth.facade";
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 
     export const authGuard: CanActivateFn = () => {
         
-        const authService = inject(AuthService);
+        const authService = inject(AuthFacade);
         const router = inject(Router);
 
         if (authService.usuarioLogado()){
