e0416903ae739ea8b3da2dc07638a47e2c84deb4
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-08-26 21:19:42 -0300

    Refactor:  src/app/core/guards/Admin.guard.ts

--- src/app/core/guards/Admin.guard.ts
+++ src/app/core/guards/Admin.guard.ts
@@ -5,15 +5,12 @@ import { AuthFacade } from "../facades/auth.facade";
 export const adminGuard: CanActivateFn = () => {
     const router = inject(Router);
     const authService = inject(AuthFacade);
-
     if(!authService.usuarioLogado()){
         return router.createUrlTree(['/login']);
     }
-
     if(!authService.admin()){
         return router.createUrlTree(['/acesso-negado']);
     }
-
     return true;
     
 }
