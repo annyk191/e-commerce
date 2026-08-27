143f26ab37347dd39fd6029eed52c2a0376c919e
Anny karolinny <annykarolinnynascimento@gmail.com>
2026-08-26 21:19:21 -0300

    Refactor: src/app/core/facades/carrinho.facade.ts

--- src/app/core/facades/carrinho.facade.ts
+++ src/app/core/facades/carrinho.facade.ts
@@ -1,32 +1,22 @@
 import { Injectable, inject } from '@angular/core';
-
 import { CarrinhoService } from '../services/carrinho.service';
-
 import { ItemCarrinho } from '../models/item-carrinho';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrinhoFacade {
-
   private carrinhoService = inject(CarrinhoService);
-
   itensCarrinho = this.carrinhoService.itens;
-
   quantidadeCarrinho = this.carrinhoService.quantidade;
-
   totalCarrinho = this.carrinhoService.total;
-
   carrinhoVazio = this.carrinhoService.carrinhoVazio;
-
   adicionarProdutoCarrinho(produto: ItemCarrinho) {
     this.carrinhoService.adicionar(produto);
   }
-
   limparCarrinho() {
     this.carrinhoService.limpar();
   }
-
   removerItem(indice: number) {
     this.carrinhoService.removerPorIndice(indice);
   }
