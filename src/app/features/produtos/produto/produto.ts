2f935e6e36512e74ecefab9db8432f5c697e6d6e
Anny karolinny <annykarolinnynasciment@gmail.com>
2026-08-31 21:07:33 -0300

    Refactor:   src/app/features/produtos/produto/produto.ts

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -1,9 +1,11 @@
-import { Component,Output,EventEmitter, Input,} from '@angular/core';
+import { Component,Output,EventEmitter, Input,inject} from '@angular/core';
 import { UpperCasePipe,} from '@angular/common';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { ItemCarrinho } from '../../../core/models/item-carrinho';
+import { FavoritosService } from '../../../core/services/favoritos.service';
+
 
 @Component({
   selector: 'app-produto',
@@ -12,23 +14,41 @@ import { ItemCarrinho } from '../../../core/models/item-carrinho';
   styleUrl: './produto.css',
 })// adicionando a classe produto com as propriedades produto, 
 export class Produto {
-  @Input() nome: string ='';
-  @Input() preco: number = 0;
-//saida de dados de produtos selecionados para lista-produtos
- @Output() produtoSelecionado = new EventEmitter<string>();
-selecionarProduto(){
-  this.produtoSelecionado.emit(this.nome);
-}
-@Output() produtoAdicionado =new EventEmitter<{
-  nome: string;
-  preco: number;
-}>();
 
-adicionarAoCarrinho() {
-  this.produtoAdicionado.emit({
-    nome:this.nome,
-    preco:this.preco,
-  });
+  private favoritosService = inject(FavoritosService);
+
+  @Input() nome = '';
+  @Input() preco = 0;
+
+  @Output() produtoSelecionado = new EventEmitter<string>();
+  @Output() produtoAdicionado = new EventEmitter<ItemCarrinho>();
+
+   selecionarProduto(): void {
+    this.produtoSelecionado.emit(this.nome);
+  }
+
+  adicionarAoCarrinho(): void {
+    this.produtoAdicionado.emit({
+      nome: this.nome,
+      preco: this.preco
+    });
+  }
+
+ adicionarFavorito(): void {
+  if (this.estaFavorito()) {
+    this.favoritosService.remover(this.nome);
+  } else {
+    this.favoritosService.adicionar(this.nome);
+  }
 }
-}
+
+  removerFavorito(): void {
+    this.favoritosService.remover(this.nome);
+  }
+
+  estaFavorito(): boolean {
+    return this.favoritosService.estaFavorito(this.nome);
+  }
+
  
+}
